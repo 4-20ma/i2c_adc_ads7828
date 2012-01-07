e48315832f156c86fb6d0242b0fb50aab016aa70
Doc Walker <2-718@wvfans.net>
2012-01-07 07:31:38 -0600

    Update docs w/v1.0.1 changes

--- class_a_d_s7828_channel.js
+++ class_a_d_s7828_channel.js
@@ -18,7 +18,7 @@ var class_a_d_s7828_channel =
     [ "commandByte_", "class_a_d_s7828_channel.html#aa32344aee61d3ba37f68c3d2fcdff9a5", null ],
     [ "device_", "class_a_d_s7828_channel.html#a00e469ff13dcaa5fa9f647ca9165cf94", null ],
     [ "index_", "class_a_d_s7828_channel.html#a34219ddd59e982d32562f094292b5274", null ],
-    [ "samples_", "class_a_d_s7828_channel.html#ad018a1b5cc00d10cd75d1c2dba897760", null ],
+    [ "samples_", "class_a_d_s7828_channel.html#a325a0244f06bcee456c98133019a8484", null ],
     [ "total_", "class_a_d_s7828_channel.html#ad59c1575c352dbbf4f82ed64f39be03e", null ],
     [ "MOVING_AVERAGE_BITS_", "class_a_d_s7828_channel.html#ac97c771263d85e4bb2f75ae9c27d624f", null ]
 ];
